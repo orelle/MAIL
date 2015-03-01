37f4101c77fa3e0911862f67385229bca9499f16
Ouissam BELHADI <ouissam.belhadi@gmail.com>
2015-03-01 17:28:12 +0100

    First commit

---
+++ MAIL_Medecin/src/fr/dauphine/mail/medecin/Medecin.java
@@ -0,0 +1,13 @@
+package fr.dauphine.mail.medecin;
+
+public class Medecin {
+
+	private String nom;
+	private String prenom;
+	
+	public Medecin() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
